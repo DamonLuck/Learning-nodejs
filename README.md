d65496c632ae8ac5190cb0da7a105c3d9e7bd27f
Damon <damonluck@gmail.com>
2017-02-09 19:24:21 +0000

    cloud 9 settings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Learning-nodejs
 
 [![Build Status](https://travis-ci.org/DamonLuck/Learning-nodejs.svg?branch=master)](https://travis-ci.org/DamonLuck/Learning-nodejs)
+
+[![bitHound Overall Score](https://www.bithound.io/github/DamonLuck/Learning-nodejs/badges/score.svg)](https://www.bithound.io/github/DamonLuck/Learning-nodejs)
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var options = { port: process.env.PORT, ip: process.env.IP };
+
+const express = require('express');
+const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  response.send('Hello from Express!');
 })
 
-app.listen(port, (err) => {
+app.listen(options.port, options.ip, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${options.port}`);
 })
